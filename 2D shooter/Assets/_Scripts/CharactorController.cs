bb9190fda53b161ae1e7e4d21df134681e52eab3
iloy1004 <iloy1004ca@gmail.com>
2016-01-26 11:57:55 -0500

    Add charactor
    add charactor and code for controlling

---
+++ 2D shooter/Assets/_Scripts/CharactorController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharactorController : MonoBehaviour {
+
+    private float _playInput;
+    private Transform _transform;
+    private Vector2 _currentPosition;
+
+    //Public instanc variables
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start () {
+        this._transform = gameObject.GetComponent<Transform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._currentPosition = this._transform.position;
+
+        this._playInput = Input.GetAxis("Horizontal");
+        Debug.Log(this._playInput);
+
+        if(this._playInput>0)
+        {
+            this._currentPosition += new Vector2(0, this.speed);
+        }
+
+        if(this._playInput<0)
+        {
+            this._currentPosition -= new Vector2(0, this.speed);
+        }
+
+        this._checkBounds();
+        
+        this._transform.position = this._currentPosition;
+	}
+
+    private void _checkBounds()
+    {
+        if (this._currentPosition.y < -290)
+        {
+            this._currentPosition.y = -290;
+        }
+
+        if (this._currentPosition.y > 290)
+        {
+            this._currentPosition.y = 290;
+        }
+    }
+}
---
+++ 2D shooter/Assets/_Scripts/LandscapeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandscapeController : MonoBehaviour {
+
+    //Public instanc variables
+    public float speed = 3;
+
+    //Private instance variables
+    private Transform _transform;
+    private Vector2 _currentPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        //Make a reference with the Transform Component
+        this._transform = gameObject.GetComponent<Transform>();
+        // Reset the landscape to the top
+        this.Reset();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._currentPosition = this._transform.position;
+        this._currentPosition -= new Vector2(this.speed,0);
+        this._transform.position = this._currentPosition;
+
+        if(this._currentPosition.x <= -978)
+        {
+            this.Reset();
+        }
+	}
+
+    void Reset()
+    {
+        this._transform.position = new Vector2(978f, 0);
+    }
+}
